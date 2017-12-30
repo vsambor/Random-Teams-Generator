2a83953dd7020a7b51448cfa32118a4a07de18bc
Vasile Sambor <vasilesambor@gmail.com>
2017-12-30 03:08:05 +0100

    Fix e2e tests

--- client/test/e2e/runner.js
+++ client/test/e2e/runner.js
@@ -27,7 +27,7 @@ devConfigPromise.then(devConfig => {
   // http://nightwatchjs.org/guide#settings-file
   let opts = process.argv.slice(2)
   if (opts.indexOf('--config') === -1) {
-    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
+    opts = opts.concat(['--config', 'client/test/e2e/nightwatch.conf.js'])
   }
   if (opts.indexOf('--env') === -1) {
     opts = opts.concat(['--env', 'chrome'])
@@ -36,12 +36,12 @@ devConfigPromise.then(devConfig => {
   const spawn = require('cross-spawn')
   const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
 
-  runner.on('exit', function (code) {
+  runner.on('exit', function(code) {
     server.close()
     process.exit(code)
   })
 
-  runner.on('error', function (err) {
+  runner.on('error', function(err) {
     server.close()
     throw err
   })
